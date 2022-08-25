4ee894e75585efe0c94d1667e63e1d9d2ecf6cb8
Chris Kahrs <chriskahrs@msn.com>
2022-08-25 10:56:09 -0400

    keybindings and 2 line graph working

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
+from imp import reload
 import streamlit as st
 import streamlit.components.v1 as components
+import pandas as pd
+import numpy as np
+import time
 
 
 def read_index_html(copy_text: int):
@@ -18,7 +22,17 @@ def left_callback():
 def right_callback():
     st.session_state.counter += 1
 
+list1 = [0,1,2,3,4]
+list2 = [1,2,4,7,st.session_state.counter+1*2]
+list3 = [st.session_state.counter+3*39,4,5,8,9]
+df = pd.DataFrame({"A": list1, "B": list2, "C": list3})
+print(df)
+df = df.append({"A": st.session_state.counter, "B": st.session_state.counter+3*4, "C": st.session_state.counter+19}, ignore_index=True)
+print(df)
 
+# list1 = [st.session_state.counter]
+# list2 = [st.session_state.counter*2]
+# df = pd.DataFrame({"A": list1, "B": list2})
 st.title("Hacking Streamlit Frontend")
 st.caption(
     "Press left / right arrow keys to simulate decrement / increment button click"
@@ -30,9 +44,37 @@ left_col.button("Decrement", on_click=left_callback)
 right_col.button("Increment", on_click=right_callback)
 
 st.metric("Counter", st.session_state.counter)
+df = df.append({"A": st.session_state.counter+1, "B": st.session_state.counter+3*4, "C": st.session_state.counter+19}, ignore_index=True)
 
+df = df.append({"A": st.session_state.counter+1, "B": st.session_state.counter+3*4, "C": st.session_state.counter+19}, ignore_index=True)
+st.line_chart(df, x="A", y=["B", "C"])
+# placeholder = st.empty()
+
+# def basic_func(sec):
+#     st.write(sec)
+
+# with placeholder:
+#     for second in range(50):
+#         time.sleep(1)
+#         basic_func(second)
+        
 components.html(
-    read_index_html(st.session_state.counter),
+    read_index_html((st.session_state.counter)),
     height=0,
     width=0,
 )
+
+# chart = st.line_chart(np.random.randn(10, 2))
+full_replacement_chart = st.empty()
+
+while True:
+    new_rows = np.random.randn(10, 2)
+
+    # Append data to the chart.
+    # chart.add_rows(new_rows)
+
+    # Just replace the chart
+    full_replacement_chart.line_chart(new_rows)
+
+    # Wait
+    time.sleep(.1)
